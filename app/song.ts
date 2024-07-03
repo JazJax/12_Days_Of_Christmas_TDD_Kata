a7251bdbe1cfd69d87ca30e001c8f4f806288d0e
jasper.jackson <jasper.jackson@madetech.com>
2024-07-03 09:46:03 +0100

    refactor

--- app/song.ts
+++ app/song.ts
@@ -1,9 +1,7 @@
 export class Song {
     SingVerse(verseNumber: number): string {
-        if (verseNumber == 1) {
-            return "On the first day of Christmas"
-        }
-        return "On the second day of Christmas"
+        let dayName: String[] = ['first','second']
+        return [`On the ${dayName[verseNumber-1]} day of Christmas`,
+        `My true love gave to me`].join('\n')
     }
-
 }
--- tests/song.spec.ts
+++ tests/song.spec.ts
@@ -6,14 +6,18 @@ import { Song } from '@/song';
 
 
 describe('song', () => {
-  it('should return the first line of the 1st verse', () => {
+  it('should return the first 2 lines of the 1st verse', () => {
     const mySong: Song = new Song
     const result: string = mySong.SingVerse(1)
-    expect(result).toBe("On the first day of Christmas")
+    expect(result).toBe(
+      ["On the first day of Christmas",
+      "My true love gave to me"].join('\n'))
   });
   it('should return the first line of the 2nd verse', () => {
     const mySong: Song = new Song
     const result: string = mySong.SingVerse(2)
-    expect(result).toBe("On the second day of Christmas")
+    expect(result).toBe(
+      ["On the second day of Christmas",
+      "My true love gave to me"].join('\n'))
   });
 });
