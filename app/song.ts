b72b7392da9a04d43b41e2283561004728ad12b2
jasper.jackson <jasper.jackson@madetech.com>
2024-07-03 12:43:26 +0100

    refactor

--- app/song.ts
+++ app/song.ts
@@ -1,18 +1,33 @@
 export class Song {
     SingVerse(verseNumber: number): string {
-        let dayName: String[] = ['first','second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eigth', 'ninth', 'tenth', 'eleventh', 'twelfth']
-        
+        let dayName: String[] = [
+            'first','second', 'third', 'fourth'
+            , 'fifth', 'sixth', 'seventh', 'eigth'
+            , 'ninth', 'tenth', 'eleventh', 'twelfth'
+        ]
         let verseLines: String[] = [
-            'Three french hens'
+            'Twelve drummers drumming'
+            , 'Eleven pipers piping'
+            , 'Ten lords a-leaping'
+            , 'Nine ladies dancing'
+            , 'Eight maids a-milking'
+            , 'Seven swans swimming'
+            , 'Six geese a-laying'
+            , 'Five gold rings'
+            , 'Four calling birds'
+            , 'Three french hens'
             , 'Two turtle doves, and'
             , 'A partridge in a pear tree'
+        ].reverse()
+
+        let resultArray: String[] = [
+            `On the ${dayName[verseNumber-1]} day of Christmas`
+            , `My true love gave to me`
         ]
-        let resultArray: String[] = [`On the ${dayName[verseNumber-1]} day of Christmas`,
-        `My true love gave to me`]
 
         while (verseNumber > 0) {
-            resultArray.push(verseLines[3 - verseNumber])
             verseNumber --
+            resultArray.push(verseLines[verseNumber])
         }
         
         return resultArray.join('\n')
