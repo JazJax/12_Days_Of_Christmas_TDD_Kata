547a93928a8ae3da7de4e62f5d9731af539a82ce
jasper.jackson <jasper.jackson@madetech.com>
2024-07-03 14:35:54 +0100

    Tidied up

--- tests/song.spec.ts
+++ tests/song.spec.ts
@@ -5,63 +5,46 @@ import * as fs from 'fs';
 // Should return the first line of the 2nd verse
 // Should return the first line of any verse
 
+const fullSong: String = fs.readFileSync('tests/fullsong.txt','utf8');
+
+function getVerse(verseNumber: number ){
+  return fullSong.split('\n\n\n')[verseNumber-1]
+}
 
 describe('SingVerse', () => {
   it('should return the first verse', () => {
     const mySong: Song = new Song
     const result: string = mySong.SingVerse(1)
-    expect(result).toBe(
-      ["On the first day of Christmas",
-        "My true love gave to me",
-      "A partridge in a pear tree"].join('\n'))
+    let expectedResult = getVerse(1)
+    expect(result).toBe(expectedResult)
   })
 
   it('should return the 2nd verse', () => {
     const mySong: Song = new Song
     const result: string = mySong.SingVerse(2)
-    expect(result).toBe(
-      ["On the second day of Christmas",
-        "My true love gave to me",
-        "Two turtle doves, and",
-        "A partridge in a pear tree"].join('\n'))
+    let expectedResult = getVerse(2)
+    expect(result).toBe(expectedResult)
   });
 
   it('should return the 3rd verse', () => {
     const mySong: Song = new Song
     const result: string = mySong.SingVerse(3)
-    expect(result).toBe(
-      ["On the third day of Christmas",
-        "My true love gave to me",
-        "Three french hens",
-        "Two turtle doves, and",
-        "A partridge in a pear tree"].join('\n'))
+    let expectedResult = getVerse(3)
+    expect(result).toBe(expectedResult)
   });
 
   it('should return the 12th verse', () => {
     const mySong: Song = new Song
     const result: string = mySong.SingVerse(12)
-    expect(result).toBe(
-      ["On the twelfth day of Christmas",
-        "My true love gave to me",
-        "Twelve drummers drumming",
-        "Eleven pipers piping",
-        "Ten lords a-leaping",
-        "Nine ladies dancing",
-        "Eight maids a-milking",
-        "Seven swans swimming",
-        "Six geese a-laying",
-        "Five gold rings",
-        "Four calling birds",
-        "Three french hens",
-        "Two turtle doves, and",
-        "A partridge in a pear tree"].join('\n'))
+    let expectedResult = getVerse(12)
+    expect(result).toBe(expectedResult)
   })
 });
 
 describe('SingSong', () => {
   it('should sing the whole song', () => {
     const mySong: Song = new Song
-    const expectedResult: String = fs.readFileSync('tests/fullsong.txt','utf8');
+    const expectedResult: String = fullSong
 
     const result: string = mySong.SingSong()
     expect(result).toBe(expectedResult)
