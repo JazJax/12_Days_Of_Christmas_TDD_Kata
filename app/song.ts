35b232cd6bce2426d21454d3f3f4abe987b04194
jasper.jackson <jasper.jackson@madetech.com>
2024-07-03 12:32:07 +0100

    refactor

--- app/song.ts
+++ app/song.ts
@@ -2,18 +2,19 @@ export class Song {
     SingVerse(verseNumber: number): string {
         let dayName: String[] = ['first','second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eigth', 'ninth', 'tenth', 'eleventh', 'twelfth']
         
+        let verseLines: String[] = ['A partridge in a pear tree', 'Two turtle doves, and', 'Three french hens']
         let resultArray: String[] = [`On the ${dayName[verseNumber-1]} day of Christmas`,
         `My true love gave to me`]
 
         if(verseNumber == 1){
-            resultArray.push('A partridge in a pear tree')
+            resultArray.push(verseLines[0])
         } else if(verseNumber == 2){
-            resultArray.push('Two turtle doves, and')
-            resultArray.push('A partridge in a pear tree')
+            resultArray.push(verseLines[1])
+            resultArray.push(verseLines[0])
         } else if(verseNumber == 3){
-            resultArray.push('Three french hens')
-            resultArray.push('Two turtle doves, and')
-            resultArray.push('A partridge in a pear tree')
+            resultArray.push(verseLines[2])
+            resultArray.push(verseLines[1])
+            resultArray.push(verseLines[0])
         }
         
         return resultArray.join('\n')
